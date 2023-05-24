520553899a133b1600a633ed993171d03153879b
peacethedominator <varchaswiaman14@gmail.com>
2023-05-24 11:22:45 +0530

    Display Categories from db

---
+++ Migrations/20230523114011_SeedCategoryTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyWeb.Migrations
+{
+    public partial class SeedCategoryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cartegories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[] { 1, "1", "Action" });
+
+            migrationBuilder.InsertData(
+                table: "Cartegories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[] { 2, "2", "Scifi" });
+
+            migrationBuilder.InsertData(
+                table: "Cartegories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[] { 3, "3", "Thriller" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cartegories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cartegories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cartegories",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
